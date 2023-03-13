2436016cdf27e7cd355a4d9cb99271a540d5bf8a
Onur Bilke <onurbilke@hotmail.com>
2023-03-14 00:19:20 +0300

    First Commit

--- Java/RecyclerView/app/src/main/java/com/ob/recyclerview/AnimalAdapter.java
+++ Java/RecyclerView/app/src/main/java/com/ob/recyclerview/AnimalAdapter.java
@@ -1,7 +1,10 @@
 package com.ob.recyclerview;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.ob.recyclerview.databinding.RowAnimalBinding;
@@ -29,6 +32,10 @@ public class AnimalAdapter extends RecyclerView.Adapter<AnimalAdapter.AnimalHold
     {
         holder.binding.imgAnimal.setImageResource(animals.get(position).getImage());
         holder.binding.txtAnimal.setText(animals.get(position).getName());
+
+        holder.itemView.setOnClickListener(view -> {
+            Toast.makeText(holder.itemView.getContext(),"OK",Toast.LENGTH_LONG).show();
+        });
     }
 
     @Override
